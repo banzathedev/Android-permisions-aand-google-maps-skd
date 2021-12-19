56cf6adb7d707ff1286608409a788cbb04a844b6
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-12-19 05:01:04 -0300

    setup koin modules

--- app/src/main/java/com/example/userpermissonsandroidstudies/di/AppModules.kt
+++ app/src/main/java/com/example/userpermissonsandroidstudies/di/AppModules.kt
@@ -1,8 +1,12 @@
 package com.example.userpermissonsandroidstudies.di
 
+import com.example.userpermissonsandroidstudies.ui.viewModel.MainViewModel
+import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
-val viewModel = module {}
+val viewModel = module {
+    viewModel { MainViewModel() }
+}
 val repository = module {}
 val useCases = module {}
 val services = module {}
