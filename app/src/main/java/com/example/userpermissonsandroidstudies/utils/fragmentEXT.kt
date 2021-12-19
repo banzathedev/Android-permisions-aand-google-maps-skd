362fb202711ef205ed267f35ba118e3dd9ad9672
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-12-19 06:14:06 -0300

    adding methods to utils

--- app/src/main/java/com/example/userpermissonsandroidstudies/utils/fragmentEXT.kt
+++ app/src/main/java/com/example/userpermissonsandroidstudies/utils/fragmentEXT.kt
@@ -1,5 +1,7 @@
 package com.example.userpermissonsandroidstudies.utils
 
+import android.view.View
+import android.view.View.*
 import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
@@ -9,4 +11,16 @@ fun FragmentActivity.replaceView(fragment: Fragment, @IdRes containerId: Int = R
     supportFragmentManager.beginTransaction()
         .replace(containerId, fragment)
         .commitNow()
+}
+
+fun View.gone() {
+    visibility = GONE
+}
+
+fun View.visible() {
+    visibility = VISIBLE
+}
+
+fun View.invisible() {
+    visibility = INVISIBLE
 }
